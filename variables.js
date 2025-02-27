3b00064aa377f32f719347b8bbf5e8196f60c812
unknown <rivoandrimaifidy0@gmail.com>
2025-02-27 16:07:34 +0300

    variables

---
+++ variables.js
@@ -0,0 +1,5 @@
+var prenom = "Rivo";
+var age = 40;
+var pays = "Madagascar";
+console.log("Mon nom est :" + prenom + " et j'ai " + age + "ans " + "je viens de " + pays);
+console.log("Bonjour! mon nom est ".concat(prenom, " J'ai ").concat(age, " de ").concat(pays));
---
+++ variables.ts
@@ -0,0 +1,5 @@
+let prenom: string = "Rivo"
+let age: number = 40
+let pays: any = "Madagascar"
+console.log("Mon nom est :" + prenom + " et j'ai " + age + "ans " + "je viens de " + pays)
+console.log(`Bonjour! mon nom est ${prenom} J'ai ${age} de ${pays}`) 
